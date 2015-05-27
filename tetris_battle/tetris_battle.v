473dcbbbdf5ffdbcdfa8325e69bd6de89c1cd311
chunyou0830 <chunyou0830@gmail.com>
2015-05-27 18:19:38 +0800

    Top Module I/O Declaration
    Top module (tetris_battle.v) I/O ports declaration.

--- tetris_battle/tetris_battle.v
+++ tetris_battle/tetris_battle.v
@@ -36,5 +36,36 @@ module tetris_battle(
 	con_out_bomb
 );
 
+	// I/O PORTS DECLARATION ----------
+
+	// System Basic
+	input clk;
+	
+	// Push Button
+	input pb_in_rst, pb_in_ctl, pb_in_vol_up, pb_in_vol_dwn;
+	
+	// Keypad
+	input [3:0] pad_col_in;
+	output [3:0] pad_row_scn;
+
+	// Display and Speaker
+	output [15:0] led_out;
+	output [18:0] ftsd_out;
+	output [4:0] speaker_out
+
+	// Connector In
+	input con_in_clk_sync;
+	input [2:0] con_in_stat;
+	input [6:0] con_in_score;
+	input [2:0] con_in_ko;
+	input con_in_bomb;
+
+	//Connector Out
+	output con_out_clk_sync;
+	output [2:0] con_out_stat;
+	output [6:0] con_out_score;
+	output [2:0] con_out_ko;
+	output con_out_bomb;
+
 
 endmodule
