faf206a109d7e40a47255955c57ac0eb2fbd68ab
Chunyou Yang <chunyou0830@gmail.com>
2015-05-28 15:49:33 +0800

    Game Status Controll FSM Combinational Logic Added

--- tetris_battle/game_status.v
+++ tetris_battle/game_status.v
@@ -3,7 +3,7 @@
 // Engineer:		Jason Yang, Jimmy Lee
 // Create Date:		14:56 05/27/2015 
 // Design Name:		Game Status Controll FSM
-// Module Name:		gaming_status 
+// Module Name:		GameStatus 
 // Project Name:	Tetris Battle
 // Target Devices:	EVS 6 FPGA Demo Board
 // Description:
@@ -20,13 +20,13 @@
 `define STAT_GAME_ING		3'b110
 `define STAT_GAME_OVER		3'b111
 
-module game_status(
+module GameStatus(
 	pb_ctl,
-	stat_sync,
-	table_in,
 	dip_players,
-	global_clk,
+	game_over,
 	game_clk,
+	global_clk,
+	stat_sync,
 	stat_out
 );
 
@@ -34,11 +34,11 @@ module game_status(
 
 	// Input Ports
 	input pb_ctl;
-	input stat_sync;
-	input table_in;
 	input dip_players;
-	input global_clk;
+	input game_over;
 	input game_clk;
+	input global_clk;
+	input stat_sync;
 
 	// Output Ports
 	output reg [2:0] stat_out;
@@ -53,22 +53,40 @@ module game_status(
 			`STAT_NORMAL:
 			begin
 				if (pb_ctl && dip_players)
+				begin
+					stat_out_next = `STAT_MATCH_ING;
+				end
 				else if (pb_ctl && ~dip_players)
+				begin
+					stat_out_next = `STAT_GAME_INITIAL;
+				end
 				else
+				begin
+					stat_out_next = `STAT_NORMAL;
+				end
 			end
 			`STAT_MATCH_ING:
 			begin
 				if(stat_sync == `STAT_MATCH_ING)
+				begin
+					stat_out_next = `STAT_MATCH_SUCCESS;
+				end
 				else if(pb_ctl)
+				begin
+					stat_out_next = `STAT_NORMAL;
+				end
 				else
+				begin
+					stat_out_next = `STAT_MATCH_ING;
+				end
 			end
 			`STAT_MATCH_CANCEL:
 			begin
-				
+				stat_out_next = `STAT_NORMAL;
 			end
 			`STAT_MATCH_SUCCESS:
 			begin
-				
+				stat_out_next = `STAT_GAME_INITIAL;
 			end
 			`STAT_GAME_INITIAL:
 			begin
